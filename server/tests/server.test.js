3910f6db4a25850e498a30f92d7ced79e5cefd04
Aaron Bell <aaronbellwebdev@gmail.com>
2017-06-04 15:03:57 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,18 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 
+const todos = [{
+    text:'First test todo'
+},
+ {
+     text:'Second test todo'
+ }             ];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+   return   Todo.insertMany(todos);
+      
+  }).then(()=>done());
 });
 describe('POST /todos',()=>{
     
@@ -25,7 +35,7 @@ describe('POST /todos',()=>{
               return done(err);
           }
               
-              Todo.find().then((todos) => {
+              Todo.find({text}).then((todos) => {
                   expect(todos.length).toBe(1);
                   expect(todos[0].text).toBe(text);
                   done();
@@ -36,7 +46,7 @@ describe('POST /todos',()=>{
     
     it('should not create todo with invalid body data' , (done) =>{
        
-        request(app)
+        request(app)//supertest
         .post('/todos')
         .send({})
         .expect(400)
@@ -44,7 +54,7 @@ describe('POST /todos',()=>{
            if(err){return done(err);} 
             
         Todo.find().then((todos)=>{
-          expect(todos.length).toBe(0);  
+          expect(todos.length).toBe(2);  
             done();
         }).catch((e) => done(e));
         });
@@ -52,6 +62,24 @@ describe('POST /todos',()=>{
         
     });
     
+   
+    
     
    });
+
+ describe('GET /todos', ()=>{
+        
+       it('should get all todos',(done)=>{
+           
+           request(app)
+           .get('/todos')
+           .expect(200)
+           .expect((res)=>{//custom
+             expect(res.body.todos.length).toBe(2);  
+           })
+           .end(done);
+           
+       }) ;
+        
+    });
     
