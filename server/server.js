9ded12cc08c684aa4abb42e906cebec2e39aa434
Aaron Bell <aaronbellwebdev@gmail.com>
2017-06-04 22:28:26 -0700

    Small modification

--- server/server.js
+++ server/server.js
@@ -1,18 +1,14 @@
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-
-var {mongoose} =require('./db/mongoose.js');
-
-var {Todo}=require('./models/todo');
-
-var {User}= require('./models/user');
-
 const {ObjectID} = require('mongodb');
 
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 var app = express();
 
-const port = process.env.PORT || 3000//heroku uses this for app.list
+const port = process.env.PORT || 3000 //heroku uses this for app.list
 
 app.use(bodyParser.json());
 
