b7ce0d7a740a923ba7515ab31fbc5d8fd20d1a45
Aaron Bell <aaronbellwebdev@gmail.com>
2017-06-06 23:30:32 -0700

    Config2

--- server/config/config.js
+++ server/config/config.js
@@ -8,3 +8,6 @@ if (env === 'development'){
    process.env.PORT = 3000; 
     process.env.MONGODB_URI = 'mongodb://localhost:27017/Todoapptest'
 }
+
+//if we reun npm test, weare using Todoapp test locally
+// if we reun npm start, we are in development environment because env will be set to development
